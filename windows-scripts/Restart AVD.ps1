c0b97823f00913c3eb94bbfb57c1f87bcc82a88f
jessevallaro <64439320+jessevallaro@users.noreply.github.com>
2023-02-07 09:39:10 -0500

    Create Restart AVD.ps1

---
+++ windows-scripts/Restart AVD.ps1
@@ -0,0 +1,12 @@
+#description: Restarts the computer
+#execution mode: Individual
+#tags: Axon, AVD, reboot, restart
+
+<#
+
+#>
+
+##### Required Variables #####
+
+##### Script Logic #####
+Restart-Computer
