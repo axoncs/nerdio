754e6d02edebefab7d9291971e23e78b511db760
jessevallaro <64439320+jessevallaro@users.noreply.github.com>
2023-02-16 11:40:51 -0500

    Create Pre SentinalOne Install VSS Fix.ps1

---
+++ windows-scripts/Pre SentinalOne Install VSS Fix.ps1
@@ -0,0 +1,12 @@
+#description: Restarts the computer
+#execution mode: Individual
+#tags: Axon, AVD, vss, S1
+
+<#
+
+#>
+
+##### Required Variables #####
+
+##### Script Logic #####
+vssadmin resize ShadowStorage /For=C: On=C: MaxSize=10%
